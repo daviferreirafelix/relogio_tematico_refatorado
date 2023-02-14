61b8cd5159916731a2fd63d03a967aa4ba42fcfa
daviferreirafelix <adavi9920@gmail.com>
2023-02-14 10:11:30 -0300

    'refatoração completa do código JS'

--- exercicio-relogio-tematico.js
+++ exercicio-relogio-tematico.js
@@ -1,39 +1,36 @@
+let relogio = document.querySelector("#relogio");
+let dia = document.querySelector("#dia");
+let saudacao = document.querySelector("#saudacao");
+let tema = document.querySelector("#tema");
+
+const semana = [
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feria",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
+];
+
 setInterval(function diaSemana() {
-  let data = new Date();
-  let hora = data.getHours();
+  const data = new Date();
+  const hora = data.getHours();
 
-  let relogio = document.querySelector("#relogio");
   relogio.textContent = data.toLocaleTimeString("pt-BR");
-
-  const semana = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feria",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-  ];
-  document.querySelector("#dia").textContent = `Hoje é dia ${data.toLocaleDateString("pt-BR")}, ${semana[data.getDay()]}`;
-
-  let saudacao = document.querySelector("#saudacao");
-  let tema = document.querySelector("#tema");
+  dia.textContent = `Hoje é dia ${data.toLocaleDateString("pt-BR")}, ${semana[data.getDay()]}`;
 
   if (hora >= 00 && hora <= 5) {
     saudacao.textContent = "Boa madrugada!";
-    tema.removeAttribute("class");
-    tema.classList.add("madrugada");
+    tema.className = 'madrugada';
   } else if (hora >= 5 && hora <= 12) {
     saudacao.textContent = "Bom dia!";
-    tema.removeAttribute("class");
-    tema.classList.add("manha");
+    tema.className = 'manha';
   } else if (hora >= 12 && hora <= 18) {
     saudacao.textContent = "Boa tarde!";
-    tema.removeAttribute("class");
-    tema.classList.add("tarde");
+    tema.className = 'tarde'
   } else if (hora >= 18) {
     saudacao.textContent = "Boa noite!";
-    tema.removeAttribute("class");
-    tema.classList.add("madrugada");
+    tema.className = 'madrugada';
   }
 }, 1000);
